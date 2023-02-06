70bfbbc1bff9b1d8f7bc41f847863e62cf129b6c
Николай Алешинков <aleshinkov91@gmail.com>
2023-02-07 00:47:48 +0300

    add_random_barrier

--- barrier.py
+++ barrier.py
@@ -3,15 +3,16 @@ from pygame.colordict import THECOLORS
 
 
 class Barrier(pygame.sprite.Sprite):
-    def __init__(self, x, speed, filename) -> None:
+    def __init__(self, x, speed, surf, group) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(filename).convert_alpha()
+        self.image = surf
         self.image.set_colorkey(THECOLORS['white'])
         self.rect = self.image.get_rect(center = (x, 0))
         self.speed = speed
+        self.add(group)
 
     def update(self, *args) -> None:
         if self.rect.y < args[0] - 20:
             self.rect.y += self.speed
         else:
-            self.rect.y = 0
+            self.kill()
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from pygame.colordict import THECOLORS
 from barrier import Barrier
  
 pygame.init()
+
+time_step = 4000
+pygame.time.set_timer(pygame.USEREVENT, time_step)
  
 W = 900
 H = 780
@@ -20,7 +23,7 @@ clock = pygame.time.Clock()
 x = W // 2
 y = H // 2
 speed = 5
-car_speed = 15
+car_speed = 5
 
 field = pygame.image.load('images/field.bmp')
 
@@ -36,48 +39,97 @@ line_2 = pygame.draw.line(road, THECOLORS['white'], (160, 0), (160, 800), 5)
 line_3 = pygame.draw.line(road, THECOLORS['white'], (308, 0), (308, 800), 5)
 line_4 = pygame.draw.line(road, THECOLORS['white'], (460, 0), (460, 800), 5)
 
-barrier_1 = Barrier(x - 150, speed, 'images/barrier_150.png')
-barrier_2 = Barrier(x, speed, 'images/barrier_150.png')
-barrier_3 = Barrier(x + 150, speed, 'images/barrier_150.png')
+balls_images = ['barrier_150.png']
+balls_surf = [pygame.image.load('images/'+path).convert_alpha() for path in balls_images]
 
+balls = pygame.sprite.Group()
 
+# def createBall(group):
+#     indx = balls_surf
+#     numbers = (range(4))
+#     selected_numbers = random.sample(numbers, k=2)
+#     x = W // 2
+#     if selected_numbers == 1:
+#         x = W // 2 - 150
+#     elif selected_numbers == 2:
+#         x = W // 2
+#     elif selected_numbers == 2:
+#         x = W // 2 + 150
+#     speed = random.randint(1, 4)
 
+def createBall(group):
+    indx = random.randint(0, len(balls_surf)-1)
+    x = random.choice([W // 2 - 150, W // 2, W // 2 + 150])
+    speed = 2
+ 
+    return Barrier(x, speed, balls_surf[indx], group)
+
+# barrier_1 = Barrier(x - 150, speed, 'images/barrier_150.png')
+# barrier_2 = Barrier(x, speed, 'images/barrier_150.png')
+# barrier_3 = Barrier(x + 150, speed, 'images/barrier_150.png')
+
+# def createBarier(group):
+#     indx = randint(0, len(balls_surf)-1)
+#     x = randint(20, W-20)
+#     speed = randint(1, 4)
+ 
+#     return Ball(x, speed, balls_surf[indx], group)
+createBall(balls)
+createBall(balls)
+transmission = 1
 while True:
     line_1 = pygame.draw.line(road, THECOLORS['white'], (10, 0), (10, 800), 5)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    
+        elif event.type == pygame.USEREVENT:
+            createBall(balls)
+            createBall(balls)
     mv = pygame.key.get_pressed()
+    # if mv[pygame.K_z]:
+    #     car_speed -= 1
+    #     if transmission < 1:
+    #         transmission = 1 
+    # elif mv[pygame.K_x]:
+    #     car_speed += 1
+
     if mv[pygame.K_LEFT]:
-        car_rect.x -= speed
-        if car_rect.x < 230:
-            car_rect.x = 230
+        car_rect.x -= car_speed
+    if car_rect.x < 230:
+        car_rect.x = 230
     elif mv[pygame.K_RIGHT]:
-        car_rect.x += speed
+        car_rect.x += car_speed
         if car_rect.x > 600:
             car_rect.x = 600
     elif mv[pygame.K_UP]:
-        car_rect.y -= speed
+        car_rect.y -= car_speed
         if car_rect.y < 0:
             car_rect.y = 0
     elif mv[pygame.K_DOWN]:
-        car_rect.y += speed
+        car_rect.y += car_speed
         if car_rect.y > 650:
             car_rect.y = 650
+
 
     sc.blit(field, (0 ,0))
     sc.blit(road, road_rect)
     sc.blit(car, car_rect)
-    sc.blit(barrier_1.image, barrier_1.rect)
-    sc.blit(barrier_2.image, barrier_2.rect)
-    sc.blit(barrier_3.image, barrier_3.rect)
-
+    balls.draw(sc)
     pygame.display.update()
+    balls.update(H)
+    # sc.blit(barrier_1.image, barrier_1.rect)
+    # sc.blit(barrier_2.image, barrier_2.rect)
+    # sc.blit(barrier_3.image, barrier_3.rect)
 
-    barrier_1.update(H)
-    barrier_2.update(H)
-    barrier_3.update(H)
+    
+    # l = list(range(1, 4))
+    # while True:
+    #     random.shuffle(l)
+    #     print(l)
+    # barrier_1.update(H)
+    # barrier_2.update(H)
+    # barrier_3.update(H)
+    
     # sc.fill(THECOLORS['white'])
     # # pygame.draw.rect(sc, THECOLORS['white'], (x, y, 10, 20))
     # rect = pygame.Rect(0, 0, 250, 250)
