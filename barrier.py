d39e0e97d2536d4ecdbd3c25e8bffc7f892abb79
Николай Алешинков <aleshinkov91@gmail.com>
2023-02-08 00:57:51 +0300

    add_paused_and fields

--- barrier.py
+++ barrier.py
@@ -3,12 +3,30 @@ from pygame.colordict import THECOLORS
 
 
 class Barrier(pygame.sprite.Sprite):
-    def __init__(self, x, speed, surf, group) -> None:
+    def __init__(self, x, speed, score, group) -> None:
         pygame.sprite.Sprite.__init__(self)
-        self.image = surf
+        self.image = pygame.image.load('images/barrier_150.png').convert_alpha()
         self.image.set_colorkey(THECOLORS['white'])
         self.rect = self.image.get_rect(center = (x, 0))
         self.speed = speed
+        self.score = score
+        self.add(group)
+
+    def update(self, *args) -> None:
+        if self.rect.y < args[0] - 20:
+            self.rect.y += self.speed
+        else:
+            self.kill()
+
+
+class Haystack(pygame.sprite.Sprite):
+    def __init__(self, x, speed, score, group) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('images/haystack_120.png').convert_alpha()
+        self.image.set_colorkey(THECOLORS['white'])
+        self.rect = self.image.get_rect(center = (x, 0))
+        self.speed = speed
+        self.score = score
         self.add(group)
 
     def update(self, *args) -> None:
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import random
 from pygame.colordict import THECOLORS
-from barrier import Barrier
+from barrier import Barrier, Haystack
  
 pygame.init()
 
@@ -15,22 +15,23 @@ H = 780
 sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("События от клавиатуры")
 # pygame.display.set_icon(pygame.image.load("app.bmp"))
- 
 
-FPS = 60        # число кадров в секунду
+
+FPS = 60
 clock = pygame.time.Clock()
  
 x = W // 2
 y = H // 2
 speed = 5
 car_speed = 5
+score = 0
 
-field = pygame.image.load('images/field.bmp')
-
+field_stage1 = pygame.image.load('images/field.bmp')
+field_stage2 = pygame.image.load('images/field_stage2.jpg')
 road = pygame.image.load('images/road.jpg')
 road_rect = road.get_rect(center = (x, y))
 
-car = pygame.image.load('images/car.png'   ).convert()
+car = pygame.image.load('images/car.png').convert()
 car.set_colorkey(THECOLORS['white'])
 car_rect = car.get_rect(center = (x, y + 320))
 
@@ -38,60 +39,54 @@ line_1 = pygame.draw.line(road, THECOLORS['white'], (10, 0), (10, 800), 5)
 line_2 = pygame.draw.line(road, THECOLORS['white'], (160, 0), (160, 800), 5)
 line_3 = pygame.draw.line(road, THECOLORS['white'], (308, 0), (308, 800), 5)
 line_4 = pygame.draw.line(road, THECOLORS['white'], (460, 0), (460, 800), 5)
+line_down = pygame.draw.line(road, THECOLORS['white'], (0, 800), (900, 800), 5)
 
-balls_images = ['barrier_150.png']
-balls_surf = [pygame.image.load('images/'+path).convert_alpha() for path in balls_images]
 
 balls = pygame.sprite.Group()
 
-# def createBall(group):
-#     indx = balls_surf
-#     numbers = (range(4))
-#     selected_numbers = random.sample(numbers, k=2)
-#     x = W // 2
-#     if selected_numbers == 1:
-#         x = W // 2 - 150
-#     elif selected_numbers == 2:
-#         x = W // 2
-#     elif selected_numbers == 2:
-#         x = W // 2 + 150
-#     speed = random.randint(1, 4)
-
 def createBall(group):
-    indx = random.randint(0, len(balls_surf)-1)
+    global score
     x = random.choice([W // 2 - 150, W // 2, W // 2 + 150])
-    speed = 2
- 
-    return Barrier(x, speed, balls_surf[indx], group)
+    speed = 3
+        
+    return Barrier(x, speed, score, group)
 
-# barrier_1 = Barrier(x - 150, speed, 'images/barrier_150.png')
-# barrier_2 = Barrier(x, speed, 'images/barrier_150.png')
-# barrier_3 = Barrier(x + 150, speed, 'images/barrier_150.png')
+def createHaystack(group):
+    global score
+    x = random.choice([W // 2 - 150, W // 2, W // 2 + 150])
+    speed = 3
+        
+    return Haystack(x, speed, score, group)
 
-# def createBarier(group):
-#     indx = randint(0, len(balls_surf)-1)
-#     x = randint(20, W-20)
-#     speed = randint(1, 4)
- 
-#     return Ball(x, speed, balls_surf[indx], group)
+
+def collideBalls():
+    global score
+    for ball in balls:
+        if line_down.collidepoint(ball.rect.center):
+            score += 5
+            ball.kill()
+
+paused = False
 createBall(balls)
 createBall(balls)
-transmission = 1
+
 while True:
-    line_1 = pygame.draw.line(road, THECOLORS['white'], (10, 0), (10, 800), 5)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
         elif event.type == pygame.USEREVENT:
-            createBall(balls)
-            createBall(balls)
+            if score <= 20:
+                createBall(balls)
+                createBall(balls)
+            elif score <= 250:
+                createHaystack(balls)
+                createHaystack(balls)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                paused = not paused
+    if paused == True:
+        continue        
     mv = pygame.key.get_pressed()
-    # if mv[pygame.K_z]:
-    #     car_speed -= 1
-    #     if transmission < 1:
-    #         transmission = 1 
-    # elif mv[pygame.K_x]:
-    #     car_speed += 1
 
     if mv[pygame.K_LEFT]:
         car_rect.x -= car_speed
@@ -111,33 +106,17 @@ while True:
             car_rect.y = 650
 
 
-    sc.blit(field, (0 ,0))
+    collideBalls()
+
+    if score <= 20:
+        sc.blit(field_stage2, (0 ,0))
+    elif score <= 250:
+        sc.blit(field_stage1, (0 ,0))
+
     sc.blit(road, road_rect)
-    sc.blit(car, car_rect)
     balls.draw(sc)
+    sc.blit(car, car_rect)
     pygame.display.update()
     balls.update(H)
-    # sc.blit(barrier_1.image, barrier_1.rect)
-    # sc.blit(barrier_2.image, barrier_2.rect)
-    # sc.blit(barrier_3.image, barrier_3.rect)
-
     
-    # l = list(range(1, 4))
-    # while True:
-    #     random.shuffle(l)
-    #     print(l)
-    # barrier_1.update(H)
-    # barrier_2.update(H)
-    # barrier_3.update(H)
-    
-    # sc.fill(THECOLORS['white'])
-    # # pygame.draw.rect(sc, THECOLORS['white'], (x, y, 10, 20))
-    # rect = pygame.Rect(0, 0, 250, 250)
-    # rect.center = (x, y)
-    # pygame.draw.rect(sc, THECOLORS['black'], rect, 5)
-    # pygame.draw.aaline(sc, THECOLORS['black'], (x, y - 350), (x -50, y + 50))
-    # pygame.draw.aaline(sc, THECOLORS['black'], (x, 800), (x, 0))
-
-    # pygame.display.update()
- 
     clock.tick(FPS)
